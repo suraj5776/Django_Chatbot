d9753d4e27df74ab3d589824fa895147942f58a5
Suraj <suraj775776@gmail.com>
2023-08-24 00:42:42 +0530

    create Chatbot using Django

---
+++ chatbot/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        n=1,
+        stop = None,
+        temperature = 0.7,  
+    )
+    # print(response)
+    answer = response.choices[0].text.strip()
+    return answer
+
+# Create your views here
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
+    return render(request,'chatbot.html')
